64870b4091ef1f9a99680ae740aae55c7dbe5935
Pierpressure <pierpressure22@gmail.com>
2015-11-30 21:30:19 -0800

    RBWR added

---
+++ RBWR.py
@@ -0,0 +1,370 @@
+from pprint import pprint
+from bs4 import BeautifulSoup
+import requests
+from splinter import Browser
+import os
+import json
+import re
+import statistics as stats
+
+
+def addRankingToExperts(expertNames, expertsRankings, playerWithRankings):
+	playerName = playerWithRankings[1].strip()
+	i = 3
+	first = playerWithRankings[i].strip()
+	# Skip the injury field if there is one
+	injuryTypes = ['P', 'Q', 'D', 'O', 'IR']
+	if first in injuryTypes:
+		i = 5
+	else:
+		i = 4
+	for expert in expertNames:
+		if playerWithRankings[i].strip() == 'NR':
+			expertsRankings[expert].append((playerName, float('inf')))
+		elif expert == "Avg":
+			expertsRankings[expert].append((playerName, float(playerWithRankings[i])))
+		else:
+			expertsRankings[expert].append((playerName, int(playerWithRankings[i])))
+		i += 1
+def addRankingToExpertsRB(expertNames, RBexpertsRankings, playerWithRankings):
+	playerName = playerWithRankings[1].strip()
+	i = 3
+	first = playerWithRankings[i].strip()
+	# Skip the injury field if there is one
+	injuryTypes = ['P', 'Q', 'D', 'O', 'IR', 'SSPD']
+	if first in injuryTypes:
+		i = 5
+	else:
+		i = 4
+	for expert in expertNames:
+		if playerWithRankings[i].strip() == 'SSPD':
+			RBexpertsRankings[expert].append((playerName, float('inf')))
+		if playerWithRankings[i].strip() == 'NR':
+			RBexpertsRankings[expert].append((playerName, float('inf')))
+		elif expert == "Avg":
+			RBexpertsRankings[expert].append((playerName, float(playerWithRankings[i])))
+		else:
+			RBexpertsRankings[expert].append((playerName, int(playerWithRankings[i])))
+		i += 1
+
+def printRankings(expertRankings):
+	for expert in expertRankings.keys():
+		if expert != "Avg":
+			print(expert + "'s Rankings:")
+			for ranking in expertRankings[expert]:
+				print(str(ranking[1]) + ". " + ranking[0])
+			print()
+	print("Average rankings for each player: ")
+	for ranking in expertRankings["Avg"]:
+		print(ranking[0] + ": " + str(ranking[1]))
+
+def getExpertRankingsQB(week, browser):
+	
+	url = "http://espn.go.com/fantasy/football/story/_/page/15ranksWeek%sQB/fantasy-football-week-%s-quarterback-rankings" %(week, week)
+
+	browser.visit(url)
+
+	table_data = browser.html
+	soup = BeautifulSoup(table_data, 'lxml')
+	headers = soup.find(class_= "tal header").find_all_next('tr')
+
+	expertNames = ["Berry", "Karabell", "Yates", "Cockroft", "Clay", "Avg"]
+	expertsRankings = {}
+	for expert in expertNames:
+		expertsRankings[expert] = []
+
+	for i in headers:
+		playerWithRankings = i.get_text(" | ").split("|")
+		# (player name, ranking)
+		addRankingToExperts(expertNames, expertsRankings, playerWithRankings)
+
+	for expert in expertsRankings.keys():
+		expertsRankings[expert].sort(key= lambda tup: tup[1])
+
+	# printRankings(expertsRankings)
+	# browser.quit()
+	return expertsRankings
+
+def getExpertRankingsRB(week, browser):
+	
+	url = "http://espn.go.com/fantasy/football/story/_/page/15ranksWeek%sRB/fantasy-football-week-%s-running-back-rankings" %(week, week)
+
+	browser.visit(url)
+
+	table_data = browser.html
+	soup = BeautifulSoup(table_data, 'lxml')
+	headers = soup.find(class_= "tal header").find_all_next('tr')
+
+	expertNames = ["Berry", "Karabell", "Yates", "Cockroft", "Clay", "Avg"]
+	RBexpertsRankings = {}
+	for expert in expertNames:
+		RBexpertsRankings[expert] = []
+
+	for i in headers:
+		playerWithRankings = i.get_text(" | ").split("|")
+		# (player name, ranking)
+		addRankingToExpertsRB(expertNames, RBexpertsRankings, playerWithRankings)
+
+	for expert in RBexpertsRankings.keys():
+		RBexpertsRankings[expert].sort(key= lambda tup: tup[1])
+
+	# printRankings(expertsRankings)
+	# browser.quit()
+	return RBexpertsRankings
+
+def getExpertRankingsWR(week, browser):
+	
+	url = "http://espn.go.com/fantasy/football/story/_/page/15ranksWeek%sWR/fantasy-football-week-%s-wide-receiver-rankings" %(week, week)
+
+	browser.visit(url)
+
+	table_data = browser.html
+	soup = BeautifulSoup(table_data, 'lxml')
+	headers = soup.find(class_= "tal header").find_all_next('tr')
+
+	expertNames = ["Berry", "Karabell", "Yates", "Cockroft", "Clay", "Avg"]
+	expertsRankings = {}
+	for expert in expertNames:
+		expertsRankings[expert] = []
+
+	for i in headers:
+		playerWithRankings = i.get_text(" | ").split("|")
+		# (player name, ranking)
+		addRankingToExperts(expertNames, expertsRankings, playerWithRankings)
+
+	for expert in expertsRankings.keys():
+		expertsRankings[expert].sort(key= lambda tup: tup[1])
+
+	# printRankings(expertsRankings)
+	# browser.quit()
+	return expertsRankings
+
+def getActualRankingsQB(week, browser):
+	url = "http://games.espn.go.com/ffl/leaders?&scoringPeriodId=%s&seasonId=2015&slotCategoryId=0" % week
+	browser.visit(url)
+
+	table_data = browser.html
+	soup = BeautifulSoup(table_data, 'lxml')
+
+	# (player name -> (position rank, fantasy points))
+	actualRankings = {}
+
+	headers = soup.find_all(class_= re.compile("^pncPlayerRow"))
+	j = 1
+	for i in headers:
+		playerStats = i.get_text(" | ").split("|")
+		# print(playerStats)
+		actualRankings[playerStats[0].strip()] = (j, float(playerStats[-1]))
+		j += 1
+	# print(actualRankings)
+	# browser.quit()
+	return actualRankings
+
+def getActualRankingsRB(week, browser):
+	url = "http://games.espn.go.com/ffl/leaders?&scoringPeriodId=%s&seasonId=2015&slotCategoryId=2" % week
+	browser.visit(url)
+
+	table_data = browser.html
+	soup = BeautifulSoup(table_data, 'lxml')
+
+	# (player name -> (position rank, fantasy points))
+	actualRankings = {}
+
+	headers = soup.find_all(class_= re.compile("^pncPlayerRow"))
+	j = 1
+	for i in headers:
+		playerStats = i.get_text(" | ").split("|")
+		# print(playerStats)
+		actualRankings[playerStats[0].strip()] = (j, float(playerStats[-1]))
+		j += 1
+	# print(actualRankings)
+	# browser.quit()
+	return actualRankings
+
+def getActualRankingsWR(week, browser):
+	url = "http://games.espn.go.com/ffl/leaders?&scoringPeriodId=%s&seasonId=2015&slotCategoryId=4" % week
+	browser.visit(url)
+
+	table_data = browser.html
+	soup = BeautifulSoup(table_data, 'lxml')
+
+	# (player name -> (position rank, fantasy points))
+	actualRankings = {}
+
+	headers = soup.find_all(class_= re.compile("^pncPlayerRow"))
+	j = 1
+	for i in headers:
+		playerStats = i.get_text(" | ").split("|")
+		# print(playerStats)
+		actualRankings[playerStats[0].strip()] = (j, float(playerStats[-1]))
+		j += 1
+	# print(actualRankings)
+	# browser.quit()
+	return actualRankings
+
+def expertScore(expertRankings, actualRankings):
+	# expertRankings: (player name, ranking)
+	# actualRankings: (player name -> (position rank, fantasy points))
+
+	# print("expert rankings")
+	# print(expertRankings)
+	# print("actual rankings: ")
+	# print(actualRankings)
+	score = 0.0
+	processed = []
+	print "EXPERT RANKS:  " + str(expertRankings)
+	print "ACTUAL RANSK:  " + str(actualRankings)
+	for i in range(len(expertRankings)):
+		currPlayer = expertRankings[i][0]
+		currPlayerExpertRanking = expertRankings[i][1]
+		processed.append(currPlayer)
+		if currPlayer in actualRankings.keys():
+			currPlayerActualRanking = actualRankings[currPlayer][0]
+			currPlayerActualPoints = actualRankings[currPlayer][1]
+		else:
+			currPlayerActualRanking = float('inf')
+			currPlayerActualPoints = 0
+		for j in range(i+1, len(expertRankings)):
+			# By nature of the expert rankings being sorted, we know that each player we
+			# are comparing to will have a worse ranking than the current one.
+			comparePlayer = expertRankings[j][0]
+			if comparePlayer in actualRankings.keys():
+				comparePlayerActualRanking = actualRankings[comparePlayer][0]
+				comparePlayerActualPoints = actualRankings[comparePlayer][1]
+			else:
+				# For simplicity, assume worst possible fantasy point total is 0.
+				comparePlayerActualRanking = float('inf')
+				comparePlayerActualPoints = 0
+			# Success
+			if currPlayerActualRanking <= comparePlayerActualRanking:
+				score += abs(currPlayerActualPoints - comparePlayerActualPoints)**2
+			# Failure
+			else:
+				score -= abs(currPlayerActualPoints - comparePlayerActualPoints)**2
+	return score/100.0
+
+def expertScoreRB(expertRankings, actualRankings):
+	# expertRankings: (player name, ranking)
+	# actualRankings: (player name -> (position rank, fantasy points))
+
+	# print("expert rankings")
+	# print(expertRankings)
+	# print("actual rankings: ")
+	# print(actualRankings)
+	score = 0.0
+	processed = []
+
+	for i in range(len(expertRankings)):
+		currPlayer = expertRankings[i][0]
+		currPlayerExpertRanking = expertRankings[i][1]
+		processed.append(currPlayer)
+		if currPlayer in actualRankings.keys():
+			currPlayerActualRanking = actualRankings[currPlayer][0]
+			currPlayerActualPoints = actualRankings[currPlayer][1]
+		else:
+			currPlayerActualRanking = float('inf')
+			currPlayerActualPoints = 0
+		for j in range(i+1, len(expertRankings)):
+			# By nature of the expert rankings being sorted, we know that each player we
+			# are comparing to will have a worse ranking than the current one.
+			comparePlayer = expertRankings[j][0]
+			if comparePlayer in actualRankings.keys():
+				comparePlayerActualRanking = actualRankings[comparePlayer][0]
+				comparePlayerActualPoints = actualRankings[comparePlayer][1]
+			else:
+				# For simplicity, assume worst possible fantasy point total is 0.
+				comparePlayerActualRanking = float('inf')
+				comparePlayerActualPoints = 0
+			# Success
+			if currPlayerActualRanking <= comparePlayerActualRanking:
+				score += abs(currPlayerActualPoints - comparePlayerActualPoints)**2
+			# Failure
+			else:
+				score -= abs(currPlayerActualPoints - comparePlayerActualPoints)**2
+	return score/100.0
+
+def getScoresForExperts(expertsRankings, actualRankings):
+	scores = []
+	for expert in expertsRankings.keys():
+		score = expertScore(expertsRankings[expert], actualRankings)
+		scores.append((expert, score))
+	return scores
+
+def getScoresForExpertsRB(expertsRankings, actualRankings):
+	scores = []
+	for expert in expertsRankings.keys():
+		score = expertScoreRB(expertsRankings[expert], actualRankings)
+		scores.append((expert, score))
+	return scores
+
+def updateEspnExpertScores(week, browser, scores):
+	expertsRankingsQB = getExpertRankingsQB(week, browser)
+	actualRankingsQB = getActualRankingsQB(week, browser)
+	weekScoresQB = getScoresForExperts(expertsRankingsQB, actualRankingsQB)
+	for tup in weekScoresQB:
+		scores[tup[0]].append(tup[1])
+
+def updateEspnExpertScoresRB(week, browser, scores):
+	expertsRankingsRB = getExpertRankingsRB(week, browser)
+	actualRankingsRB = getActualRankingsRB(week, browser)
+	weekScoresRB = getScoresForExpertsRB(expertsRankingsRB, actualRankingsRB)
+	for tup in weekScoresRB:
+		scores[tup[0]].append(tup[1])
+
+def updateEspnExpertScoresWR(week, browser, scores):
+	expertsRankingsWR = getExpertRankingsWR(week, browser)
+	actualRankingsWR = getActualRankingsWR(week, browser)
+	weekScoresWR = getScoresForExperts(expertsRankingsWR, actualRankingsWR)
+	for tup in weekScoresWR:
+		scores[tup[0]].append(tup[1])
+
+
+def main():
+	browser = Browser('chrome')
+# Quarterback Calculations
+	scores = {}
+	for expert in ["Berry", "Karabell", "Yates", "Cockroft", "Clay", "Avg"]:
+		scores[expert] = []
+	for i in range(5,6):
+		print("Calculating ESPN expert QB scores for Week %s" %i)
+		updateEspnExpertScores(i, browser, scores)
+	finalScores = []
+	for expert in scores.keys():
+		expertScoreMean = stats.mean(scores[expert])
+		finalScores.append((expert, expertScoreMean))
+	print("Final Average QB Scores for Each Expert:")
+	for final in finalScores:
+		print(final[0] + " : " + str(final[1]))
+# Running Back Calculations
+	# RBscores = {}
+	# for expert in ["Berry", "Karabell", "Yates", "Cockroft", "Clay", "Avg"]:
+	# 	RBscores[expert] = []
+	# for i in range(5,6):
+	# 	print("Calculating ESPN expert RB scores for Week %s" %i)
+	# 	updateEspnExpertScoresRB(i, browser, RBscores)
+	# RBfinalScores = []
+	# for expert in RBscores.keys():
+	# 	RBexpertScoreMean = stats.mean(RBscores[expert])
+	# 	RBfinalScores.append((expert, RBexpertScoreMean))
+	# print("Final Average RB Scores for Each Expert:")
+	# for final in RBfinalScores:
+	# 	print(final[0] + " : " + str(final[1]))
+# WR CALCulation
+	# WRscores = {}
+	# for expert in ["Berry", "Karabell", "Yates", "Cockroft", "Clay", "Avg"]:
+	# 	WRscores[expert] = []
+	# for i in range(5,6):
+	# 	print("Calculating ESPN expert WR scores for Week %s" %i)
+	# 	updateEspnExpertScoresWR(i, browser, WRscores)
+	# WRfinalScores = []
+	# for expert in WRscores.keys():
+	# 	WRexpertScoreMean = stats.mean(WRscores[expert])
+	# 	WRfinalScores.append((expert, WRexpertScoreMean))
+	# print("Final Average WR Scores for Each Expert:")
+	# for final in WRfinalScores:
+	# 	print(final[0] + " : " + str(final[1]))
+	browser.quit()
+
+main()
+
+
